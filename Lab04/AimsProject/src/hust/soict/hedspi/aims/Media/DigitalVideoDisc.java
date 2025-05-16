7d15470765ebf716046b231e9455636d5f9b7a2c
trongcong125 <daotrongcong11@gmail.com>
2025-05-16 20:37:18 +0700

    Lab04

---
+++ Lab04/AimsProject/src/hust/soict/hedspi/aims/Media/DigitalVideoDisc.java
@@ -0,0 +1,40 @@
+package hust.soict.hedspi.aims.Media;
+
+public class DigitalVideoDisc extends Media {
+    private static int nbDigitalVideoDiscs = 0; // Track total DVDs
+    private String director;
+    private int length;
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        super(++nbDigitalVideoDiscs, title, category, cost); // gọi constructor Media
+        this.director = director;
+    }
+    public DigitalVideoDisc(String title, String category, float cost) {
+        super(++nbDigitalVideoDiscs, title, category, cost);
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        super(++nbDigitalVideoDiscs, title, category, cost);
+        this.director = director;
+        this.length = length;
+    }
+
+    public DigitalVideoDisc(String title) {
+        super(++nbDigitalVideoDiscs, title, "", 0);
+    }
+    public String getDirector() {
+        return director;
+    }
+    public void setDirector(String director) {
+        this.director = director;
+    }
+    public int getLength() {
+        return length;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+    public void play() {
+        System.out.println("Playing DVD: " + this.getTitle());
+        System.out.println("DVD length: " + this.getLength());
+}
+}
