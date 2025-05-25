2b6d8e4d019c89925a03b707c8e50423abcd57b1
trongcong125 <daotrongcong11@gmail.com>
2025-05-26 02:36:51 +0700

    Delete Lab_01 directory

--- Lab_01/Bai222/bai2_22.java
+++
@@ -1,9 +0,0 @@
-package Bai222;
-import javax.swing.JOptionPane;
-public class bai2_22 {
-	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Hello word! How are you?");
-		System.exit(0);
-	}
-
-}
